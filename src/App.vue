c59903bb9fa2a9b4a9806a3cf52d83ee745fc68b
JeanyAurellia <jeanyaurellia@gmail.com>
2023-09-29 20:17:57 +0700

    update todolist

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,13 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    
-  </header>
-
+  <nav>
+      <RouterLink to="#" style="font-family: 'Salsa', cursive;
+; font-size: 25px; font-weight: 600; text-decoration: underline; color: black">TODO NOW</RouterLink>
+    <div class="kanan">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/todo">To Do</RouterLink>
+    </div>
+  </nav>
   <RouterView />
 </template>
